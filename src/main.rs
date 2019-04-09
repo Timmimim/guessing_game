57c6d7d60d2a1a18c8d50ea18bf3d086128f2700
Timm Kühnel <t_kueh06@uni-muenster.de>
2019-04-09 22:15:04 +0200

    Finished Matching Game, finished Chapter 2 of The Book 2nd Edition.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,50 @@
 extern crate rand;
 
 use std::io;
-use rand::Rng;
+use std::cmp::Ordering;
+use rand::Rng; // methods are built on "traits", which need be in the scope
 
 fn main() {
+	println!("");
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
 
-    io::stdin().read_line(&mut guess)
-    	.expect("Failed to read line...");
+	    println!("Please input your guess.");
 
-    println!("The secret number is: {}", secret_number);
+	    let mut guess = String::new();
 
-	println!("You guessed: {}", guess);
+	    io::stdin().read_line(&mut guess)
+	    	.expect("Failed to read line...");
 
-	/*
-		Commentary, my dear Watson. 
-	*/
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("\nErm.. NaN!?\n");
+				continue;
+			}
+		};
 
+
+		println!("You guessed: {}", guess);
+
+		/*
+			Commentary, my dear Watson. 
+		*/
+		// very well, sir...
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
+				println!("You win!");
+				break;
+			}
+		}
+		println!("");
+	}
+	println!("The secret number is: {}", secret_number);
 }
